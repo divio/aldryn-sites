0e4c390a3e41e68ca0cf5ad20990c0f6231cf926
Stefan Foulis <stefan@foulis.ch>
2014-12-17 08:41:44 +0100

    cleanup

--- aldryn_sites/conf.py
+++ aldryn_sites/conf.py
@@ -13,8 +13,6 @@ class AldrynSitesConf(AppConf):
     # TODO: validate settings
 
     def configure(self):
-        print self.configured_data['AUTO_CONFIGURE_ALLOWED_HOSTS']
-        print self.configured_data['DOMAINS']
         if self.configured_data['AUTO_CONFIGURE_ALLOWED_HOSTS'] and self.configured_data['DOMAINS']:
             s = self._meta.holder
             ALLOWED_HOSTS = s.ALLOWED_HOSTS
