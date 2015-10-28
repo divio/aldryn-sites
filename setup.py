6eecb85938eca7ad4eb22ff7a6684eb8bf1446ae
Mykhailo Kolesnyk <mykhailo.kolesnyk@divio.ch>
2015-10-28 18:50:34 +0200

    Add Django 1.5 support
    This also sets proper exit code of the test runner command. As a result
    we see that some tests fail on Django 1.7. We'll have to fix them later.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=(
-        'Django>=1.6,<1.8',
+        'Django>=1.5,<1.8',
         'YURL>=0.13',
         'django-appconf',
     ),
